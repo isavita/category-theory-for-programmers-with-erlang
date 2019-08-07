4ffa11dd9ffc77e6a909ccf36e19bdfd5af117ce
Aleksandar Dimov <isavitaisa@gmail.com>
2019-08-07 13:19:19 +0100

    Add 5.6 and 5.7

--- ch5.erl
+++ ch5.erl
@@ -1,5 +1,5 @@
 -module(ch5).
--export([number_or_string/1]).
+-export([number_or_string/1, m/1]).
 
 %% 5.1. Show that the terminal object is unique up to unique isomorphism.
 % Let's assume that there two terminal object A and B.
@@ -28,5 +28,20 @@ number_or_string({right, Str}) -> Str.
 %% Hint: Define a function
 -spec m(either()) -> number.
 m({left, N}) -> N;
-m({left, true}) -> 1;
-m({left, false}) -> 0;
+m({right, true}) -> 1;
+m({right, false}) -> 0.
+
+%% 5.6. Continuing the previous problem: How would you argue that int
+%% with the two injections i and j cannot be “better” than Either ?
+% The reason is the when we map to int instead of Either we are losing information.
+% Therefore we do not have way to invert the injection due to the fact that the boolean
+% values have been map to integer and at the same time we have integer that are going to same
+% values. Thus the injections i and j to int cannot be "better" than Either.
+
+%% 5.7. Still continuing: What about these injections?
+%% int i(int n) { if (n < 0) return n; return n + 2; }
+%% int j(bool b) { return b? 0: 1; }
+% The same apply for the above injection if we are talking for finite sets, which is the case of int in C++.
+% The reasons is due to the fact that our initial values are N + 2 (N integers and two boolean values) but the
+% result set is only N. The "lost" is caused by ((max:int - 1) + 2) and (max:int + 2)
+% they actually map to (min:int) and (min:int -1).
